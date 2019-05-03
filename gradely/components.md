f403f19b212a02de6fbf6b87b9ac77b6cc0079ce
Babatunde Calebs O <calebsconsults@gmail.com>
2019-05-03 16:30:12 +0100

    Gradely dummy

---
+++ gradely/components.md
@@ -0,0 +1,38 @@
+<div class="dropdown-menu widget_select_menu">
+    <header class="widget_select_header">
+        <div class="select_menu_search">
+            <span class="search_icon">
+                <svg class="cust_icon" viewBox="0 0 22 22" xmlns="http://www.w3.org/2000/svg">
+                <g transform="translate(1 1)" fill="none" fill-rule="evenodd">
+                    <path d="M15.364 2.636A9 9 0 1 1 2.636 15.363 9 9 0 0 1 15.364 2.636zM20 20l-5-5"></path>
+                </g>
+                </svg>
+            </span>
+            <input class="form-control prefixed_input" placeholder="Enter contact name..." title="Enter contact name...">
+        </div>
+    </header>
+    <!-- Toggle the class "is_empty" when the list is empty -->
+    <div class="widget_select_body ">
+        <ul class="search_result_list">
+            <li class="search_result_item">
+                <a href="" class="result_item_wrapper active">
+                    <div class="user_profile_details">
+                        <div class="user_info_avi">
+                            <img src="../img/" alt="">
+                            <span class="default_avi">RH</span>
+                        </div>
+                        <div class="user_info">
+                            <h4 class="title text_clipped font-bold">Rubius Hagrid</h4>
+                        </div>
+                    </div>
+                </a>
+            </li>
+        </ul>
+        <div class="search_result_empty">
+            <p>No result found</p>
+        </div>
+    </div>
+    <footer class="widget_select_footer">
+        <button type="button" class="widgetSelectCTA" data-target="#new_child" data-toggle="modal">Add a Child</b>
+    </footer>
+</div>
---
+++ gradely/js/main.js
@@ -0,0 +1,90 @@
+jQuery(document).ready(function($){
+  $('[data-toggle="tooltip"]').tooltip()
+
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		scroll_top_duration = 2000,
+        $back_to_top = $('.to-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('is-visible') : $back_to_top.removeClass('is-visible fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('fade-out');
+		}
+	});
+    
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+        }, scroll_top_duration
+		);
+	});
+    
+    /* General Navigation*/
+
+    //primary navigation slide-in effect
+    // Toggles the top-navs fixed and scroll state.
+
+      var headerHeight = $('.mainHeader').height();
+      $(window).on('scroll', {
+            previousTop: 0
+        }, 
+        function () {
+          var currentTop = $(window).scrollTop();
+          //check if user is scrolling up
+          if (currentTop < this.previousTop ) {
+            //if scrolling up...
+            if (currentTop > 0 && $('.mainHeader').hasClass('is-fixed')) {
+              $('.mainHeader').addClass('is-visible');
+            } else {
+              $('.mainHeader').removeClass('is-visible is-fixed');
+            }
+          } else {
+            //if scrolling down...
+            $('.mainHeader').removeClass('is-visible');
+            if( currentTop > headerHeight && !$('.mainHeader').hasClass('is-fixed')) $('.mainHeader').addClass('is-fixed');
+          }
+          this.previousTop = currentTop;
+      });
+        
+        //open/close primary navigation
+    $('.primary-nav-trigger').on('click', function(event){
+        event.stopPropagation();
+        $('.menu-icon').toggleClass('is-clicked');
+        $('.mainHeader').toggleClass('menu-is-open');
+        $('.mainHeader').toggleClass('is-active');
+        $('body').toggleClass('scroll_lock');
+    });
+        
+        // Show and hide password, user login
+    var visualTrigger = $('#showPass');
+    var pwd_input = $('.user_pass');
+    visualTrigger.on('click',function() {
+      $(this).toggleClass('show_pass');
+      if (pwd_input.attr("type") == "password") {
+          pwd_input.attr("type", "text");
+      } else {
+          pwd_input.attr("type", "password");
+      }
+    });
+
+    /*Show more collapsed items in a list with more than 4 items*/
+    var show_more_toggle = $(".collapsetoggle");
+        show_more_toggle.on('click', function() {
+        $(this).parents(".collapsibleBlock").toggleClass('blockCollapsed');
+    });
+
+     /*Custom Selector*/
+     var selector = $(".pillSelectControl"), 
+        selectorLabel = $(".pillWrapper");
+     
+        selector.on('change', function() {
+          $(this).parents(".selectorItem").toggleClass("selected");
+      });
+
+});
